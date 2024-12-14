3bcd03e35e8768787a001f5b406cef72426e5fb3
Mufique Patwary <mdmusfiquepatwary@gmail.com>
2024-12-15 00:16:28 +0600

    rendered cartitems

--- main.js
+++ main.js
@@ -56,9 +56,11 @@ const product = {
   let currentColor = product.colors[0].code;
   let selectedColor = product.colors.find((item) => item.code === currentColor);
   let quantity = 0;
-  let currentSize = product.wrist_sizes[0].label;
-  const selectedSize = product.wrist_sizes.find(item => item.label === currentSize);
+  let currentSize = product.wrist_sizes[0];
+  const selectedSize = product.wrist_sizes.find(item => item.label === currentSize.label);
   const cartItems = [];
+  let priceTotal = 0; 
+  let quantityTotal = 0;
   
 
   const handleSelectedColor = (color,inx) => {
@@ -77,7 +79,8 @@ const product = {
   }
 
   const handleSelectedSize = (size,inx) => {
-    currentSize = size
+    console.log(size);
+    currentSize = size;
     const allSizes = document.querySelectorAll('.sizes');
     const labels = document.querySelectorAll('.label');
     labels.forEach((item,idx) => {
@@ -101,9 +104,6 @@ const product = {
   }
 
   const handleAddToCart = (id,image,name,color,size,qty,price) => {
-
-    console.log(image,name,color,size,qty,price);
-
     if(quantity < 1){
       alert('please add an item first'); 
       return
@@ -114,7 +114,7 @@ const product = {
       image,
       name,
       color,
-      size,
+      size : size.label,
       qty,
       price
     }
@@ -128,22 +128,84 @@ const product = {
     }else{
       cartItems.push(products)
     }
+    const cartTotalDiv = document.querySelector('#cart-count');
+    cartTotalDiv.innerHTML = cartItems.length; 
     alert('Succesfully added');
+    
   }
   
+  
+  const cartItemsRender = () => {
+    const cartItemDiv = document.querySelector('#cart-items');
+    const cartDiv = document.createElement('div');
+    cartItemDiv.innerHTML = "";
+    const cartItemHtml = cartItems.map((item) =>  `
+       <div class='grid grid-cols-8 gap-2 py-4 border-b items-center '>
+              <div class='col-span-4 gap-2 flex items-center'>
+              <img src=${item.image} alt=${item.name} class='h-9 w-9 '/>
+              <p class='text-nowrap text-sm text-[#364A63]'>${item.name}</p>
+              </div>
+              <p class='text-sm text-[#364A63]'>${item.color}</p>
+              <p class='text-sm text-[#364A63] font-bold leading-[23.1px]'>${item.size}</p>
+              <p class='text-sm text-[#364A63] font-bold leading-[23.1px]'>${item.qty}</p>
+              <p class='text-sm text-[#364A63] font-bold leading-[23.1px]'>$${item.price * item.qty}.00</p>
+        </div>
+      
+            `
+      ).join('');
+
+
+    cartDiv.innerHTML = `
+      <div class='md:max-w-[651px] md:w-auto w-[651px] pr-5 md:pr-0 text-center overflow-x-auto md:overflow-x-hidden'>
+          <div class='grid grid-cols-8 gap-3 border-b pb-4 text-[#8091A7] text-sm '>
+            <div class='text-start'>Item</div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div>Color</div>
+            <div>Size</div>
+            <div>Qnt</div>
+            <div>Price</div>
+          </div>  
+  
+         ${cartItemHtml}
+          <div>
+            <div class='grid grid-cols-8 items-center mt-5'>
+              <div class='col-span-6 text-start text-[#364A63] text-base font-bold'>Total</div>
+                <p id="quantity-total" class='col-span-1 text-center text-[#364A63] text-sm font-bold'></p>
+                <p id="price-total" class='col-span-1 text-center text-lg text-[#364A63] font-bold'></p>
+              
+            </div>
+            <div class='flex flex-1 justify-end gap-3 mt-5'>
+              <button class='border border-[#DBDFEA] rounded-sm px-4 py-2 focus:outline-none text-[#364A63] text-[13px] font-bold' onClick={onClose}>Continue Shopping</button>
+              <button class='bg-[#6576FF] px-3 py-2 text-white  rounded-md focus:outline-none  text-[13px] font-bold'>Checkout</button>
+            </div>
+          </div>
+        </div>
+    `;
+    cartItemDiv.appendChild(cartDiv);
+    priceTotal = cartItems.reduce((prev,curr) => prev + curr.price * curr.qty,0);
+    quantityTotal = cartItems.reduce((prev,curr) => prev + curr.qty,0);
+    const priceTotalDiv = document.querySelector('#price-total');
+    const quantityTotalDiv = document.querySelector('#quantity-total');
+    quantityTotalDiv.innerHTML = `${quantityTotal}`
+    priceTotalDiv.innerHTML = `$${priceTotal}.00`
+  }
+
 
   
   
+  
 
-  const renderProduct = (product) => {
+const renderProduct = (product) => {
     const section = document.getElementById("product-section");
 
     // Main product content
     section.innerHTML = `
-      <div class="w-fit ">
-        <img src="${selectedColor.image}" alt="${product.name}" class="h-auto selected-image w-auto lg:h-[720px] lg:max-w-[600px]">
+      <div >
+        <img src="${selectedColor.image}" alt="${product.name}" class="h-auto selected-image w-auto flex-1 lg:h-[720px] lg:max-w-[600px]">
       </div>
-      <div class="flex flex-col justify-start space-y-5">
+      <div class="flex flex-col flex-1 justify-start space-y-5">
         <h2 class="text-[40px] text-[#364A63] font-bold">${product.name}</h2>
         <div class="flex gap-2 items-center">
           <div class="stars" data-rating="${product.rating}"></div>
@@ -203,6 +265,7 @@ const product = {
                 
               </button>
             </div>
+            <div id="cart-items"></div>
       </div>
     `;
 
@@ -223,6 +286,7 @@ const product = {
     //append sizes
     const sizes = document.getElementById("size-options");
     product.wrist_sizes.forEach((size,idx) => {
+      // console.log(size);
       const sizeDiv = document.createElement("div");
       sizeDiv.innerHTML = `
         <div class="flex gap-3 border px-[18px] py-2 cursor-pointer sizes ${idx === 0 ? "border-[#6576FF]" : "border-[#DBDFEA]"}">
@@ -242,7 +306,7 @@ const product = {
         updateQuantity();
       }
     
-      const handleDecrement = () => {
+    const handleDecrement = () => {
         if(quantity > 0){
             quantity -= 1;
             updateQuantity();
@@ -251,11 +315,14 @@ const product = {
     const updateQuantity = () => {
         quantityInput.value = quantity; 
     };
+
     document.getElementById('plus').addEventListener('click',handleIncrement)
     document.getElementById('minus').addEventListener('click',handleDecrement);
-    document.getElementById('cartBtn').addEventListener('click',() => handleAddToCart(product.id,selectedColor.image,product.name,selectedColor.name,currentSize.label,quantity,currentSize.price));
+    document.getElementById('cartBtn').addEventListener('click',() => 
+      {handleAddToCart(product.id,selectedColor.image,product.name,selectedColor.name,currentSize,quantity,currentSize.price),cartItemsRender()});
+};
 
-  };
+
 
   // Initialize product rendering
   renderProduct(product);
